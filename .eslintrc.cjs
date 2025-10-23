f03cb41b0975af0691fd112d8d7b879886e55ce9
iftype <iftype@naver.com>
2025-10-24 00:37:17 +0900

    chore(config): 스타일 라이브러리 설정
    - package.json: eslint와 prettier 종속성 추가
    - .eslintrc.js: 컨벤션을 위해 규칙 설정
    - .prettierc.js: 일관된 포매팅 규칙
    - index.js: 포매팅 적용
    - ApplicationTest.js: 포매팅적용

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  ignorePatterns: ['package*', '.npmrc', '*.md', '.*', '__tests__'],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['acc', 'e'] }],
+    'class-methods-use-this': 'warn',
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+};
