d3eabbdda882b99d1de91d8e3f98d094a437d950
iftype <iftype@naver.com>
2025-10-24 06:24:16 +0900

    feat(Validator): Validtor클래스 구현 및 테스트 추가
    - true/false만 반환하는 유틸 클래스 구현
    - 순수함수로만 구성하여 재사용하기위함

---
+++ src/validator/Validatior.js
@@ -0,0 +1,32 @@
+class Validator {
+  static isBlank(str) {
+    return str.trim() === '';
+  }
+
+  static isLong(str, max) {
+    return str.length > max;
+  }
+
+  static isSymbol(str) {
+    return !/^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/.test(str);
+  }
+
+  static isNegative(num) {
+    return num < 0;
+  }
+
+  static isConvertNumber(param) {
+    if (param === null || typeof param === 'undefined') {
+      return false;
+    }
+    const strValue = String(param).trim();
+    if (strValue === '') {
+      return false;
+    }
+    if (Number.isNaN(Number(strValue))) {
+      return false;
+    }
+    return true;
+  }
+}
+export default Validator;
