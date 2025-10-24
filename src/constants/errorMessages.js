2ecaefda6bff16757c9356e36827a51f94dd80e6
iftype <iftype@naver.com>
2025-10-25 01:29:22 +0900

    fix(ValidatiorTest): 파일 이름 수정 및 에러 메세지 수정
    - Validatior -> ValidatiorTest 파일 이름 수정
    - errorMessage: 숫자입니다 -> 숫자여야 합니다 변경

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -5,7 +5,7 @@ const ERROR_MESSAGES = {
   LONG: '이름은 5자 이하로 입력해주세요.',
   DUPLICATE: '중복된 이름이 존재합니다',
   SYMBOL: '이름은 한글, 영어, 숫자만 가능합니다.',
-  NUM: '입력 값이 숫자입니다',
+  NUM: '입력 값이 숫자여야 합니다',
   NEGATIVE: '입력 값이 음수입니다',
 };
 export default ERROR_MESSAGES;
