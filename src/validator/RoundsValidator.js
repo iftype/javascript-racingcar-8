830118aac99eecd4240b819c4c1de0f96cb5fe11
iftype <iftype@naver.com>
2025-10-25 08:55:14 +0900

    refactor(RoundsValidator): Round Validator 이름 변경
    - round와 rounds의 구분을 위해 rounds로 이름변경
    - 변경 전 : RoundValidator
    - 변경 후 : RoundsValidator

---
+++ src/validator/RoundsValidator.js
@@ -0,0 +1,29 @@
+import ExceptionFactory from './ExceptionFactory.js';
+import Validator from './Validatior.js';
+
+class RoundsValidator {
+  static blank(lap) {
+    if (Validator.isBlank(lap)) {
+      ExceptionFactory.blank();
+    }
+  }
+
+  static num(lap) {
+    if (!Validator.isConvertNumber(lap)) {
+      ExceptionFactory.num();
+    }
+  }
+
+  static negative(lap) {
+    if (Validator.isNegative(lap)) {
+      ExceptionFactory.negative();
+    }
+  }
+
+  static validate(lap) {
+    this.blank(lap);
+    this.num(lap);
+    this.negative(lap);
+  }
+}
+export default RoundsValidator;
