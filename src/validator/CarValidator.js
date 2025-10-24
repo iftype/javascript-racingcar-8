3ab81ed658d2428a962eeaad9523efdb7d7391a4
iftype <iftype@naver.com>
2025-10-25 08:36:26 +0900

    feat(CarValidator): 중복을 검사하는 duplicate 메서드 추가

---
+++ src/validator/CarValidator.js
@@ -0,0 +1,37 @@
+import ExceptionFactory from './ExceptionFactory.js';
+import Validator from './Validatior.js';
+
+class CarValidator {
+  static blank(name) {
+    if (Validator.isBlank(name)) {
+      ExceptionFactory.blank();
+    }
+  }
+
+  static length(name) {
+    if (Validator.isLong(name, 5)) {
+      ExceptionFactory.long();
+    }
+  }
+
+  static symbol(name) {
+    if (Validator.isSymbol(name)) {
+      ExceptionFactory.symbol();
+    }
+  }
+
+  static duplicate(nameList) {
+    const deleteDuplicate = new Set(nameList);
+    if (deleteDuplicate.size !== nameList.length) ExceptionFactory.duplicate();
+  }
+
+  static validate(nameList) {
+    this.duplicate(nameList);
+    nameList.forEach((name) => {
+      this.blank(name);
+      this.length(name);
+      this.symbol(name);
+    });
+  }
+}
+export default CarValidator;
