d4b33908a26f35b442ba1b1970f215f5018b108f
iftype <iftype@naver.com>
2025-10-25 08:46:26 +0900

    feat(Race): 게임 로직을 담당하는 Race 클래스 생성
    - start: 자동차 인스턴스와 경기 수를 받아 레이싱을 진행하고 결과를 받아오는 메서드, 결과를 객체 형식으로 반환
    - racing: 레이싱을 진행하는 로직, 경기 결과를 저장한 배열을 반환
    - findWinner: 우승자를 찾는 메서드, 배열로 반환

---
+++ src/domain/Race.js
@@ -0,0 +1,31 @@
+class Race {
+  start(carList, rounds) {
+    const logs = [];
+    for (let round = 0; round < rounds; round += 1) {
+      const roundResult = this.racing(carList);
+      logs.push(roundResult);
+    }
+    const winnerList = this.findWinner(logs);
+    return { logs, winnerList };
+  }
+
+  racing(carList) {
+    const roundResult = [];
+    carList.forEach((car) => {
+      car.move();
+      const { name, distance } = car.getData();
+      roundResult.push({ name, distance });
+    });
+    return roundResult;
+  }
+
+  findWinner(logs) {
+    const lastLog = logs[logs.length - 1];
+
+    const distances = lastLog.map((log) => log.distance);
+    const winnerDistance = Math.max(...distances);
+
+    return lastLog.filter((log) => log.distance >= winnerDistance).map((log) => log.name);
+  }
+}
+export default Race;
