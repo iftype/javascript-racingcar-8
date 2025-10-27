81a29f2e0f7c31347620cc6ed0a6eaa6b41b14aa
iftype <iftype@naver.com>
2025-10-27 13:17:50 +0900

    test(InputView): inputView 테스트 추가
    - mockQuestions을 재활용하여 입력한 값이 제대로 반환되는지 체크

---
+++ __tests__/view/InputViewTest.js
@@ -0,0 +1,31 @@
+import InputView from '../../src/view/InputView';
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { TEST_INPUT_NAMES, TEST_NAMES, TEST_ROUNDS } from '../constants/MockData';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe('InputView 테스트', () => {
+  let mockView;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockView = new InputView();
+  });
+
+  test('carNames 반환체크', async () => {
+    mockQuestions([TEST_NAMES]);
+    const userInput = await mockView.carNames();
+    expect(userInput).toEqual(TEST_INPUT_NAMES);
+  });
+
+  test('rounds 반환체크', async () => {
+    mockQuestions([TEST_ROUNDS]);
+    const userInput = await mockView.rounds();
+    expect(userInput).toEqual(TEST_ROUNDS);
+  });
+});
