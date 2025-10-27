89f57f06cb51d389df249b05c522a31421d242b9
iftype <iftype@naver.com>
2025-10-27 13:16:55 +0900

    test(CarFactory): CarFactory 테스트 로직 추가

---
+++ __tests__/domain/CarFactory.js
@@ -0,0 +1,23 @@
+import Car from '../../src/domain/Car';
+import CarFactory from '../../src/domain/CarFactory';
+import MoveStrategy from '../../src/domain/strategy/MoveStrategy';
+
+class MockStrategy extends MoveStrategy {
+  constructor() {
+    super();
+  }
+}
+describe('CarFactory 검사', () => {
+  let mockStrategy;
+  let mockFactory;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockStrategy = new MockStrategy();
+    mockFactory = new CarFactory(mockStrategy);
+  });
+
+  test('생성되는지 테스트', () => {
+    const newCar = mockFactory.createCars(['a'])[0];
+    expect(newCar).toBeInstanceOf(Car);
+  });
+});
--- src/domain/strategy/MoveStrategy.js
+++ src/domain/strategy/MoveStrategy.js
@@ -1,12 +1,12 @@
 class MoveStrategy {
   constructor() {
     if (new.target === MoveStrategy) {
-      MoveStrategy.abstract();
+      throw new Error('MoveStrategy를 상속받아야합니다');
     }
   }
 
   move() {
-    MoveStrategy.abstractMethod();
+    throw new Error('메소드가 구현 전입니다');
   }
 }
 export default MoveStrategy;
